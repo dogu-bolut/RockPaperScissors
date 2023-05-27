7bb255b0be07a516a4b0682758b58656b4f099b8
Mr.Stijket <mr.stijket@gmail.com>
2023-05-27 12:05:51 +0300

    Fully Recreated

--- main.js
+++ main.js
@@ -7,52 +7,101 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 updateScoreElement();
 
 /*
-if(!score){
-    score = {
-        wins: 0,
-        losses: 0,
-        ties: 0
-    }
+if (!score) {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
 }
 */
 
-localStorage.getItem('score');
-let playerMove = "";
-function generateRandomNumber(current, beat, lose) {
-    const randomNumber = Math.random();
-    let computerMove = "";
-    let result = "";
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = "Rock";
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = "Paper";
-    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        computerMove = "Scissors";
+let isAutoPlaying = false;
+let intervalID;
+
+function autoPlay() {
+    if (!isAutoPlaying) {
+        intervalID = setInterval(function () {
+            const playerMove = pickComputerMove();
+            playGame(playerMove)
+        }, 1000);
+        isAutoPlaying = true;
+    } else {
+        clearInterval(intervalID);
+        isAutoPlaying = false;
+    }
+}
+
+function playGame(playerMove) {
+    const computerMove = pickComputerMove();
+
+    let result = '';
+
+    if (playerMove === 'scissors') {
+        if (computerMove === 'rock') {
+            result = 'You Lose.';
+        } else if (computerMove === 'paper') {
+            result = 'You Win.';
+        } else if (computerMove === 'scissors') {
+            result = 'Tie.';
+        }
+
+    } else if (playerMove === 'paper') {
+        if (computerMove === 'rock') {
+            result = 'You Win.';
+        } else if (computerMove === 'paper') {
+            result = 'Tie.';
+        } else if (computerMove === 'scissors') {
+            result = 'You Lose.';
+        }
+
+    } else if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie.';
+        } else if (computerMove === 'paper') {
+            result = 'You lose.';
+        } else if (computerMove === 'scissors') {
+            result = 'You win.';
+        }
     }
 
-    if (computerMove === current) {
-        updateResult("Tie.");
-        score.ties++;
-    } else if (computerMove === beat) {
-        updateResult("You Win.");
-        score.wins++;
-    } else if (computerMove === lose) {
-        updateResult("You Lose.");
-        score.losses++;
+    if (result === 'You Win.') {
+        score.wins += 1;
+    } else if (result === 'You Lose.') {
+        score.losses += 1;
+    } else if (result === 'Tie.') {
+        score.ties += 1;
     }
 
     localStorage.setItem('score', JSON.stringify(score));
 
     updateScoreElement();
-    document.querySelector('.js-moves').innerHTML = `
-    You <img src="${playerMove}-emoji.png" class="move-icon">
-    <img src="${computerMove}-emoji.png" class="move-icon"> Computer
-    `;
-}
-function updateScoreElement() {
-    document.querySelector('.js-score').innerHTML = `
-        Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
-}
-function updateResult(result) {
+
     document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = `You
+  <img src="${playerMove}-emoji.png" class="move-icon">
+  <img src="${computerMove}-emoji.png" class="move-icon">
+  Computer`;
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score')
+        .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+        computerMove = 'scissors';
+    }
+
+    return computerMove;
 }
